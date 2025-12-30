9c3187baf57b01ae78cfa2b68d14e350c190fff1
ojrocket <nsmixd14jashwanth@gmail.com>
2025-12-30 16:02:56 +0530

    added the cocktail and the about section with some scroll trigger animation

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,18 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from './components/navbar.jsx';
 import Hero from './components/hero.jsx';
+import Cocktails from './components/Cocktails.jsx';
+import About from './components/about.jsx';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
     return (
         <main>
-            <Navbar/>
-            <Hero/>
+            <Navbar />
+            <Hero />
+            <Cocktails />
+            <About />
         </main>
     )
  }
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,19 +1,22 @@
-import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
-import { useMediaQuery } from "react-responsive";
-import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import { useRef, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 
 
 const Hero = () => {
   const videoRef = useRef();
 
   const isMobile = useMediaQuery({maxWidth: 768});
+
+
   useGSAP(() => {
-    const heroSplit = new SplitText('.tile', { type: 'chars,words' });
+    const heroSplit = new SplitText('.title', { type: 'chars, words' });
     const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
 
     heroSplit.chars.forEach((char, i) => char.classList.add('text-gradient')) ;
+
       gsap.from(heroSplit.chars, {
         yPercent: 100,
         duration: 1.8,
@@ -36,37 +39,37 @@ const Hero = () => {
         start: 'top top',
         end: 'bottom top',
         scrub: true,
-      }
-    })
+      },
+      })
       .to('.hero-right-leaf', {y: 200},0)
       .to('.hero-left-leaf', {y: -200},0)
-   const startValue = isMobile ? 'top 50%' : 'center 60%';   
-    const endValue = isMobile ? '120% top' : 'bottom top';     
+      const startValue = isMobile ? 'top 50%' : 'center 60%';   
+      const endValue = isMobile ? '120% top' : 'bottom top';     
 
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: 'video',
+      let tl = gsap.timeline({
+       scrollTrigger: {
+        trigger: '.video',
         start: startValue,
         end: endValue,
         scrub: true,
         pin : true,
-        }
-    })
+        },
+     });
      videoRef.current.onloadedmetadata = () => {
       tl.to(videoRef.current,{
         currentTime : videoRef.current.duration,
-      })
-     }
+      });
+     };
   } , []);
     return (
       <>
        <section id="hero" className="noisy">
         <h1 className="tile">RTG</h1>
+
         <img src="/images/hero-left-leaf.png"
         alt="hero left leaf"
         className="hero-left-leaf"/>
 
-        
         <img src="/images/hero-right-leaf.png"
         alt="hero right leaf"
         className="hero-right-leaf"/>
@@ -77,19 +80,27 @@ const Hero = () => {
               <p> COOL. CRISP. CLASSIC</p>
               <p className="subtitle">Sip the spirit <br /> of Summer </p>
             </div>
+
             <div className="view-cocktails">
               <p className="subtitle">Every cocktail on our menu is a blend of premium ingredientsl, creative
           flair, and timeless recipes — designed to delight your senses .</p>
-          <a href="#cocktails">View Cocktails</a>
+              <a href="#cocktails">View Cocktails</a>
             </div>
           </div>
         </div>
        </section>
+
        <div className="video absolute inset-0">
-        <video ref= {videoRef} src= "/videos/output.mp4" muted playsInline preload="auto" />
+        <video 
+        ref= {videoRef} 
+        muted
+        playsInline 
+        preload="auto"
+        src= "/videos/output.mp4"
+        />
        </div>
       </>
-    )
-}
+    );
+};
 
 export default Hero;
