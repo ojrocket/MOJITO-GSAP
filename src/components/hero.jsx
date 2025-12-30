1370d13ec3f1d790138fc8467ff2bbad70a6eb47
ojrocket <nsmixd14jashwanth@gmail.com>
2025-12-30 12:10:33 +0530

    added the scroll trigger animation to the video on the hero section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,9 +1,14 @@
+import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
+import { useMediaQuery } from "react-responsive";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
 
 
 const Hero = () => {
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({maxWidth: 768});
   useGSAP(() => {
     const heroSplit = new SplitText('.tile', { type: 'chars,words' });
     const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
@@ -35,6 +40,23 @@ const Hero = () => {
     })
       .to('.hero-right-leaf', {y: 200},0)
       .to('.hero-left-leaf', {y: -200},0)
+   const startValue = isMobile ? 'top 50%' : 'center 60%';   
+    const endValue = isMobile ? '120% top' : 'bottom top';     
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'video',
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin : true,
+        }
+    })
+     videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current,{
+        currentTime : videoRef.current.duration,
+      })
+     }
   } , []);
     return (
       <>
@@ -63,6 +85,9 @@ const Hero = () => {
           </div>
         </div>
        </section>
+       <div className="video absolute inset-0">
+        <video ref= {videoRef} src= "/videos/output.mp4" muted playsInline preload="auto" />
+       </div>
       </>
     )
 }
