c4585c2de5d6c724490b48fd4772c70f0b7ae721
ojrocket <nsmixd14jashwanth@gmail.com>
2025-12-31 11:52:44 +0530

    added the art section with scroll triggers

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/navbar.jsx';
 import Hero from './components/hero.jsx';
 import Cocktails from './components/Cocktails.jsx';
 import About from './components/about.jsx';
+import Art from './components/Art.jsx';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -14,6 +15,7 @@ const App = () => {
             <Hero />
             <Cocktails />
             <About />
+            <Art />
         </main>
     )
  }
